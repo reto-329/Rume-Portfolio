187e83ca18fa85fea3e5e81161f92b5d0bf3e2fb
reto-329 <reromotabele4love@gmail.com>
2025-09-25 19:37:38 +0100

    Initial commit with EJS setup and working Express server

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ app.use(express.static('public'));
 // Add CORS headers
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
